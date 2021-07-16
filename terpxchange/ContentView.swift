77eda7416496c67ddc7027d41155a7ed486caf4c
Alexander Quinto <alexander.e.quinto@gmail.com>
2021-07-16 02:01:22 -0400

    adding files

---
+++ terpxchange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  terpxchange
+//
+//  Created by Alexander Quinto on 7/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ terpxchange/terpxchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  terpxchangeApp.swift
+//  terpxchange
+//
+//  Created by Alexander Quinto on 7/16/21.
+//
+
+import SwiftUI
+
+@main
+struct terpxchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
